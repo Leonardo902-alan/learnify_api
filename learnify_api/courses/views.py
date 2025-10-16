976e0173d8243447f00b2aa263c880ea6aae0f57
Leonardo902-alan <leonardo.alanya@tecsup.edu.pe>
2025-10-15 23:05:51 -0500

    5

--- learnify_api/courses/views.py
+++ learnify_api/courses/views.py
@@ -2,9 +2,11 @@ from rest_framework import viewsets
 from .models import Course, Instructor
 from .serializers import CourseSerializer, InstructorSerializer
 
+
 class CourseViewSet(viewsets.ModelViewSet):
     queryset = Course.objects.all()
     serializer_class = CourseSerializer
+
 
 class InstructorViewSet(viewsets.ModelViewSet):
     queryset = Instructor.objects.all()
